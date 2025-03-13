809b2127375478d997f708111061c0e1940e8d08
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-03-13 16:39:46 -0300

    Operadores aritmeticos em java

---
+++ src/fundamentos/OperadoresAritmeticos.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 6;
+
+        System.out.println(a / b );
+        System.out.println(a / (double) b);
+
+        System.out.println(a % b);
+    }
+}
