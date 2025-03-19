fff4db1d06c280cb12f9da355ae8f329a587ceaf
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-03-19 16:35:05 -0300

    Operadores Unarios e Ternarios

---
+++ src/fundamentos/operadores/OperadoresUnarios.java
@@ -0,0 +1,14 @@
+package fundamentos.operadores;
+
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        a++;
+        b--;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
