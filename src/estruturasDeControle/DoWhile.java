4105d0bd25c53e7914ddae110c72c5aba5aa3b00
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-03-27 16:50:12 -0300

    Uso de Do While

--- src/estruturasDeControle/DoWhile.java
+++ src/estruturasDeControle/DoWhile.java
@@ -1,7 +1,24 @@
 package estruturasDeControle;
 
+import java.util.Scanner;
+
 public class DoWhile {
     public static void main(String[] args) {
-        
+        Scanner consoleIn = new Scanner(System.in);
+
+        String answer = "";
+
+        do{
+            System.out.print("Type something: ");
+            answer = consoleIn.nextLine();
+            System.out.println(answer);
+
+
+            System.out.print("Do you wanna quit? [Yes][No]");
+            answer = consoleIn.nextLine();
+
+        }while(!answer.equalsIgnoreCase("yes"));
+
+        consoleIn.close();
     }
 }
