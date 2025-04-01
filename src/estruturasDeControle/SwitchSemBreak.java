a1588450f651b5bc716c36a57fd355735edbd189
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-01 17:04:11 -0300

    Uso de switch sem break

---
+++ src/estruturasDeControle/SwitchSemBreak.java
@@ -0,0 +1,17 @@
+package estruturasDeControle;
+
+public class SwitchSemBreak {
+    public static void main(String[] args) {
+
+        String color = "blac";
+
+        switch(color){
+            case "black":
+            System.out.println("kcalb");
+            case "white":
+            System.out.println("etihw");
+            default:
+            System.out.println("None");
+        }
+    }
+}
