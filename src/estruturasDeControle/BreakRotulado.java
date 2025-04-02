a74a150eb094260bd6817a42655189b0952b2932
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-02 16:38:32 -0300

    Usando labels em for

--- src/estruturasDeControle/BreakRotulado.java
+++ src/estruturasDeControle/BreakRotulado.java
@@ -3,10 +3,13 @@ package estruturasDeControle;
 public class BreakRotulado {
     public static void main(String[] args) {
         outter: for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.printf("%d %d", i, j);
+            
+            System.out.printf("%d", i);
+            if (i == 4){
+
                 break outter;
             }
+            
         }
     }
 }
