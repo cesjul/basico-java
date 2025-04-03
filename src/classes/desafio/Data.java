48d7780529a6ff28541fdfddd4454fd51a97a1be
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-03 16:44:03 -0300

    Resolvendo desafio classe Data

---
+++ src/classes/desafio/Data.java
@@ -0,0 +1,7 @@
+package classes.desafio;
+
+public class Data {
+    int day;
+    String month;
+    int year;
+}
---
+++ src/classes/desafio/DataTeste.java
@@ -0,0 +1,14 @@
+package classes.desafio;
+
+public class DataTeste {
+    public static void main(String[] args) {
+        Data data1 = new Data();
+
+        data1.day = 30;
+        data1.month = "September";
+        data1.year = 2025;
+
+        System.out.printf("%d of %s of %d", 
+                         data1.day, data1.month, data1.year);
+    }
+}
