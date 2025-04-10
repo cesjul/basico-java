a5ba223254697d7dc2eac88b7436b375d1fcd2d7
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-10 16:01:41 -0300

    Acessando atributo não static dentro de um método static

---
+++ src/classes/desafio/InserirNoMetodo.java
@@ -0,0 +1,13 @@
+package classes.desafio;
+
+public class InserirNoMetodo {
+
+    int a = 2;
+    
+    public static void main(String[] args) {
+       InserirNoMetodo p1 = new InserirNoMetodo();
+       int a = p1.a;
+
+       System.out.println(a);
+    }
+}
