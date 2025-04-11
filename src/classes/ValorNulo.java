895cfa8cab47fc0bcb08e446fa7f29da0fe164b0
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-11 14:39:21 -0300

    Criando objetos com valor null

---
+++ src/classes/ValorNulo.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class ValorNulo {
+    public static void main(String[] args) {
+        String a = "";
+        System.out.println(a);
+
+        String b = null;
+        System.out.println(b);
+    }
+}
