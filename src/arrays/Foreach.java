232f899229e8ff318704094ad36f430d08c35701
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-17 16:23:00 -0300

    Usando for each

---
+++ src/arrays/Foreach.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Foreach {
+    public static void main(String[] args) {
+        double[] a = {1.0, 2.6, 3.4};
+
+        for (double d : a) {
+            System.out.println(d);
+        }
+    }
+}
