b34a41b6d58a25eb703ef3e472ac1aca16d8ac31
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-23 17:55:23 -0300

    Usando matrizes

---
+++ src/arrays/Matriz.java
@@ -0,0 +1,49 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Matriz {
+    public static void main(String[] args) {
+        Scanner consoleIn = new Scanner(System.in);
+
+        System.out.println("How many students? ");
+        int numberOfStudents = consoleIn.nextInt();
+
+        System.out.println("How many grades per student? ");
+        int numberOfGrades = consoleIn.nextInt();
+
+        double[][] classGrades = new double[numberOfStudents][numberOfGrades];
+
+        int i = 0;
+        for (double[] student: classGrades) {
+            int j = 0;
+            for(double grade: student){
+                System.out.printf("Insert %d grade for %d student: ", 
+                                                 j+1,          i+1);
+
+                double sentGrade = consoleIn.nextDouble();
+                classGrades[i][j] = sentGrade;
+
+                j++;
+            }
+            i++;
+            if (i < numberOfStudents) {
+                System.out.println("Next student...");
+            }
+        }
+
+        // System.out.println(Arrays.toString(classGrades));
+       
+        for (int h = 0; h < numberOfStudents; h++) {
+            double total = 0;
+            for (int g = 0; g < numberOfGrades; g++){
+                total += classGrades[h][g];
+            }
+            System.out.printf("%d student avarage: %.2f\n",
+                                          h + 1,      total/numberOfGrades
+                                          );
+        }
+        consoleIn.close();
+    }
+}
