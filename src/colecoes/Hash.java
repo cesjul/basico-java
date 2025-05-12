336d4b6508c305a4609c22d05b65188749500ff3
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-05-12 17:33:12 -0300

    Usando Hashset para rápida pesquisa de elementos

---
+++ src/colecoes/Hash.java
@@ -0,0 +1,15 @@
+package colecoes;
+
+import java.util.HashSet;
+
+public class Hash {
+    public static void main(String[] args) {
+        HashSet<Usuario> users = new HashSet<>();
+
+        users.add(new Usuario("James"));
+        users.add(new Usuario("Jack"));
+        users.add(new Usuario("Will"));
+
+        System.out.println(users.contains(new Usuario("Will")));
+    }
+}
