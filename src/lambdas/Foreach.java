6ce2124cc5d673e3a2e9e1860170e1ab60d2ff88
cesjul <juliocesarsouzacarvalho284@gmail.com>
2025-07-14 12:42:27 -0300

    Referenciando outros metodos com lambda e method reference

--- src/lambdas/Foreach.java
+++ src/lambdas/Foreach.java
@@ -15,5 +15,17 @@ public class Foreach {
         System.out.println("\nMethod Reference: \n");
 
         list.forEach(System.out::println);
+
+        System.out.println("\nLambda Expression: \n");
+
+        list.forEach(item -> recieveItem(item));
+
+         System.out.println("\nMethod Reference: \n");
+
+        list.forEach(Foreach::recieveItem);
+    }
+
+    static void recieveItem(String item){
+        System.out.println("This is the item: " + item);
     }
 }
